c30159d01e8406570566feb22f017b644cdfbbf8
Samuel Lucidi <slucidi@newstex.com>
2013-04-25 00:03:01 -0400

    Adding comments

--- banker.erl
+++ banker.erl
@@ -1,13 +1,20 @@
+% Author: Sam Lucidi
+% SE441 Concurrency: Banker's Algorithm
 -module(banker).
 -export([start/1, status/0, attach/1, detach/0, request/1, release/1]).
 
-%% Client %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%% Client API %%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Spawn a banker process with a starting capital of Capital,
+% and register it to the atom 'banker'
 start(Capital) ->
 	Banker = spawn(fun () -> loop(Capital, [], []) end),
 	register(bank, Banker),
 	Banker.
 
+% Request to attach calling process to the bank with a spending limit.
 attach(Limit) ->
 	bank ! {self(), attach_, Limit},
 	receive
@@ -15,6 +22,7 @@ attach(Limit) ->
 		Any -> io:format("~p~n", [Any])
 	end.
 
+% Request to detach calling process from the bank.
 detach() ->
 	bank ! {self(), detach_},
 	receive
@@ -22,26 +30,34 @@ detach() ->
 		Any -> io:format("~p~n", [Any])
 	end.
 
+% Request that the bank's status be sent to the calling process.
 status() ->
 	bank ! {self(), status_},
 	receive
 		Any -> io:format("~p~n", [Any])
 	end.
 
+% Request to withdraw NUnits from the bank.
+% Will block if it would be unsafe to grant the request.
 request(NUnits) ->
 	bank ! {self(), request_, NUnits},
 	receive
 		Any -> io:format("~p~n", [Any])
 	end.
 
+% Release NUnits of funds back to the bank.
 release(NUnits) ->
 	bank ! {self(), release_, NUnits},
 	receive
 		Any -> io:format("~p~n", [Any])
 	end.
 
-%% Server %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% Server %%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Main loop of execution for the bank server.
+% Respond to messages from clients forever.
 loop(Capital, Clients, Defered) ->
 	receive
 		{Pid, attach_, Limit} ->
@@ -77,9 +93,11 @@ loop(Capital, Clients, Defered) ->
 		{Pid, status_} -> Pid ! {ok, s_status(Capital, Clients)}, loop(Capital, Clients, Defered)
 	end.
 
+% Check whether any defered unsafe requests
+% from clients are now safe to grant, and
+% grant them if possible.
 check_defered(Capital, Clients, Defered) ->
 	h_check_defered(Capital, Clients, Defered, []).
-
 h_check_defered(_Capital, Clients, [], NewDefered) ->
 	{Clients, NewDefered};
 h_check_defered(Capital, Clients, [ {Pid, NUnits} | T], NewDefered) ->
@@ -113,7 +131,8 @@ check_safety(Capital, Clients, NUnits) ->
 		false	-> unsafe
 	end.
 
-% Request money from the bank
+% request handler. Attempt to grant money to a client,
+% blocking the client if it would be unsafe to grant the request.
 s_request(Capital, Clients, Pid, NUnits) ->
 	Result = h_request(Capital, [], Clients, Pid, NUnits),
 	case Result of
@@ -121,6 +140,7 @@ s_request(Capital, Clients, Pid, NUnits) ->
 		{defer, _Message}			-> Result;
 		NewClients 					-> {ok, NewClients}
 	end.
+% request handler helper function.
 h_request(_Capital, _NewClients, [], _Pid, _NUnits) -> {error, "Banker: Client not attached."};
 h_request(Capital, NewClients, [{Pid, Limit, Loan} | Tail], Pid, NUnits) ->
 	Result = check_safety(Capital, NewClients ++ [{Pid, Limit, Loan + NUnits}] ++ Tail, NUnits),
@@ -131,20 +151,23 @@ h_request(Capital, NewClients, [{Pid, Limit, Loan} | Tail], Pid, NUnits) ->
 h_request(Capital, NewClients, [H | T], Pid, NUnits) ->
 	h_request(Capital, [H | NewClients], T, Pid, NUnits).
 
-% Release a held loan
+% release handler. Attempt to release a held loan.
 s_release(Capital, Clients, Pid, NUnits) ->
 	Result = h_release(Capital, [], Clients, Pid, NUnits),
 	case Result of 
 		{error, _Message}			-> Result;
 		NewClients 					-> {ok, NewClients}
 	end.
+% release handler helper function.
 h_release(_Capital, _NewClients, [], _Pid, _NUnits) -> {error, "Banker: Client not attached."};
 h_release(_Capital, NewClients, [{Pid, Limit, Loan} | Tail], Pid, NUnits) ->
 	NewClients ++ [{Pid, Limit, Loan - NUnits}] ++ Tail;
 h_release(Capital, NewClients, [H | T], Pid, NUnits) ->
 	h_release(Capital, [H | NewClients], T, Pid, NUnits).
 
-% Attach a new client to the bank.
+% attach handler. Attach a new client process to the bank as long
+% as their requested limit is not greater than the bank's capital,
+% as that would be unsafe.
 s_attach(Capital, _Clients, _Pid, Limit) when Limit > Capital ->
 	{"error", "Client is too demanding."};
 s_attach(_Capital, Clients, Pid, Limit) ->
@@ -153,13 +176,15 @@ s_attach(_Capital, Clients, Pid, Limit) ->
 		{error, _Message}	-> Result;
 		NewClients 			-> {ok, NewClients}
 	end.
+% attach handler helper function.
 h_attach(NewClients, [], Pid, Limit) ->
 	[ {Pid, Limit, 0} | NewClients ];
 h_attach(_NewClients, [{Pid, _L, _Loan} | _T], Pid, _Limit) -> {error, "Banker: Already attached."};
 h_attach(NewClients, [H | T], Pid, Limit) ->
 	h_attach([H | NewClients], T, Pid, Limit).
 
-% Detach a client from the bank.
+% detach handler. Detach a client from the bank, returning
+% that client's funds to the bank's cash on hand.
 s_detach(Clients, Pid) ->
 	Result = h_detach([], Clients, Pid),
 	case Result of
@@ -171,7 +196,7 @@ h_detach(NewClients, [{Pid, _Limit, _Loan} | Tail], Pid) -> NewClients ++ Tail;
 h_detach(NewClients, [H | T], Pid) ->
 	h_detach([H | NewClients], T, Pid).
 
-% Return the capital, cash on hand, and number of clients of the bank.
+% status handler. Return the capital, cash on hand, and number of clients of the bank.
 s_status(Capital, []) ->
 	{Capital, Capital, 0};
 s_status(Capital, Clients) ->
