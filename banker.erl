77511d74f0809d9c8494bf4b8573262045544173
Samuel Lucidi <slucidi@newstex.com>
2013-04-24 20:08:40 -0400

    Each of the basic banking commands works

--- banker.erl
+++ banker.erl
@@ -1,75 +1,132 @@
 -module(banker).
--export([start/1, status/0, attach/1, detach/0, request/1, release/1, loop/2, test/0]).
+-export([start/1, status/0, attach/1, detach/0, request/1, release/1, loop/3, sort_by_claim/1]).
 
 %% Client %%
 
 start(Capital) ->
-	Banker = spawn(banker, loop, [Capital, []]),
+	Banker = spawn(banker, loop, [Capital, [], []]),
 	register(bank, Banker),
 	Banker.
 
 attach(Limit) ->
 	bank ! {self(), attach_, Limit},
 	receive
-		Any -> Any
+		{ok} -> io:format("Client ~p attached successfully with limit ~p.~n", [self(), Limit]);
+		Any -> io:format("~p~n", [Any])
 	end.
 
 detach() ->
 	bank ! {self(), detach_},
 	receive
-		Any -> Any
+		{ok} -> io:format("Client ~p detached.~n", [ self() ] );
+		Any -> io:format("~p~n", [Any])
 	end.
 
 status() ->
 	bank ! {self(), status_},
 	receive
-		Any -> Any
+		Any -> io:format("~p~n", [Any])
 	end.
-
-test() ->
-	hi.
 
 request(NUnits) ->
 	bank ! {self(), request_, NUnits},
 	receive
-		Any -> Any
+		Any -> io:format("~p~n", [Any])
 	end.
 
 release(NUnits) ->
 	bank ! {self(), release_, NUnits},
 	receive
-		Any -> Any
+		Any -> io:format("~p~n", [Any])
 	end.
 
 %% Server %%
 
-loop(Capital, Clients) ->
+loop(Capital, Clients, Defered) ->
 	receive
 		{Pid, attach_, Limit} ->
-			Result = s_attach(Clients, Pid, Limit),
+			Result = s_attach(Capital, Clients, Pid, Limit),
 			case Result of
-				{ok, NewClients} 	-> Pid ! {ok}, loop(Capital, NewClients);
-				Any					-> Pid ! Any, loop(Capital, Clients)
+				{ok, NewClients} 	-> Pid ! {ok}, loop(Capital, NewClients, Defered);
+				Any					-> Pid ! Any, loop(Capital, Clients, Defered)
 			end;
 		{Pid, detach_} ->
 			Result = s_detach(Clients, Pid),
 			case Result of
-				{ok, NewClients} 	-> Pid ! {ok}, loop(Capital, NewClients);
-				Any					-> Pid ! Any, loop(Capital, Clients)
+				{ok, NewClients} 	-> Pid ! {ok}, loop(Capital, NewClients, Defered);
+				Any					-> Pid ! Any, loop(Capital, Clients, Defered)
 			end;
-		% {Pid, request_, NUnits} ->
-		% {Pid, release_, NUnits} ->
-		{Pid, status_} -> Pid ! {ok, s_status(Capital, Clients)}, loop(Capital, Clients)
+		{Pid, request_, NUnits} ->
+			Result = s_request(Capital, Clients, Pid, NUnits),
+			case Result of
+				{ok, NewClients}	-> Pid ! {ok}, loop(Capital, NewClients, Defered);
+				Any					-> Pid ! Any, loop(Capital, Clients, Defered)
+			end;
+		{Pid, release_, NUnits} ->
+			Result = s_release(Capital, Clients, Pid, NUnits),
+			case Result of
+				{ok, NewClients}	-> Pid ! {ok}, loop(Capital, NewClients, Defered);
+				Any					-> Pid ! Any, loop(Capital, Clients, Defered)
+			end;
+		{Pid, status_} -> Pid ! {ok, s_status(Capital, Clients)}, loop(Capital, Clients, Defered)
 	end.
 
+sort_by_claim([]) -> [];
+sort_by_claim(Clients) ->
+	Sorter = fun({ClientA, LimitA, LoanA}, {ClientB, LimitB, LoanB}) -> {LimitA-LoanA, ClientA} =< {LimitB-LoanB, ClientB} end,
+	lists:sort(Sorter, Clients).
+
+cash_on_hand(Capital, []) -> Capital;
+cash_on_hand(Capital, Clients) ->
+	Capital - lists:sum([Loan || {_Client, _Limit, Loan} <- Clients]).
+
+check_safety(_Capital, [], _NUnits) -> safe;
+check_safety(Capital, Clients, NUnits) ->
+	[LeastClaim | T] = sort_by_claim(Clients),
+	case LeastClaim =< cash_on_hand(Capital, Clients) of
+		true	-> check_safety(Capital, T, NUnits);
+		false	-> unsafe
+	end.
+
+s_request(Capital, Clients, Pid, NUnits) ->
+	Result = h_request(Capital, [], Clients, Pid, NUnits),
+	case Result of 
+		{error, _Message}			-> Result;
+		NewClients 					-> {ok, NewClients}
+	end.
+h_request(_Capital, _NewClients, [], _Pid, _NUnits) -> {error, "Client not attached."};
+h_request(Capital, NewClients, [{Pid, Limit, Loan} | Tail], Pid, NUnits) ->
+	Result = check_safety(Capital, NewClients ++ [{Pid, Limit, Loan}] ++ Tail, NUnits),
+	case Result of
+		safe 	-> NewClients ++ [{Pid, Limit, Loan + NUnits}] ++ Tail;
+		unsafe	-> {error, "Unsafe to grant request."}
+	end;
+h_request(Capital, NewClients, [H | T], Pid, NUnits) ->
+	h_request(Capital, [H | NewClients], T, Pid, NUnits).
+
+s_release(Capital, Clients, Pid, NUnits) ->
+	Result = h_release(Capital, [], Clients, Pid, NUnits),
+	case Result of 
+		{error, _Message}			-> Result;
+		NewClients 					-> {ok, NewClients}
+	end.
+h_release(_Capital, _NewClients, [], _Pid, _NUnits) -> {error, "Client not attached."};
+h_release(_Capital, NewClients, [{Pid, Limit, Loan} | Tail], Pid, NUnits) ->
+	NewClients ++ [{Pid, Limit, Loan - NUnits}] ++ Tail;
+h_release(Capital, NewClients, [H | T], Pid, NUnits) ->
+	h_release(Capital, [H | NewClients], T, Pid, NUnits).
+
 % Atach a new client to the bank.
-s_attach(Clients, Pid, Limit) ->
+s_attach(Capital, _Clients, _Pid, Limit) when Limit > Capital ->
+	{"error", "Client is too demanding."};
+s_attach(_Capital, Clients, Pid, Limit) ->
 	Result = h_attach([], Clients, Pid, Limit),
 	case Result of
 		{error, _Message}	-> Result;
 		NewClients 			-> {ok, NewClients}
 	end.
-h_attach(NewClients, [], Pid, Limit) -> [ {Pid, Limit, 0} | NewClients ];
+h_attach(NewClients, [], Pid, Limit) ->
+	[ {Pid, Limit, 0} | NewClients ];
 h_attach(_NewClients, [{Pid, _L, _Loan} | _T], Pid, _Limit) -> {error, "Already attached."};
 h_attach(NewClients, [H | T], Pid, Limit) ->
 	h_attach([H | NewClients], T, Pid, Limit).
@@ -90,4 +147,4 @@ h_detach(NewClients, [H | T], Pid) ->
 s_status(Capital, []) ->
 	{Capital, Capital, 0};
 s_status(Capital, Clients) ->
-	{Capital, Capital - lists:sum([Loan || {_Client, _Limit, Loan} <- Clients]), length(Clients)}.
+	{Capital, cash_on_hand(Capital, Clients), length(Clients)}.
--- client.erl
+++ client.erl
@@ -2,8 +2,22 @@
 -export([start/2]).
 
 start(Limit, N) ->
-	banker:attach(self, Limit),
+	banker:attach(Limit),
 	io:format("Attached ~p ~p~n", [N, self()]),
-	receive
-		Any -> io:format("~p", Any)
+	for(N, fun(_) -> banker:status() end),
+	banker:detach(),
+	io:format("Detached ~p ~p~n", [N, self()]).
+
+for(N, Fun) ->
+	for(N, 0, Fun).
+for(_N, _N, _Fun) -> ok;
+for(N, Count, Fun) ->
+	Fun(Count),
+	for(N, Count + 1, Fun).
+
+do_some_banking(Limit, Loan).
+	Choice = random:uniform(2),
+	case Choice of
+		1 -> banker:request(), do_some_banking(Limit, Loan);
+		2 -> banker:release(), do_some_banking(Limit, Loan)
 	end.
